dad245f06a76c456fbe5806a0dfa50596d37036f
Joeri Bakker <bakkerjoeri@gmail.com>
2017-01-04 15:35:14 +0100

    Added body back
    Signed-off-by: Joeri Bakker <bakkerjoeri@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -2,32 +2,60 @@
 
 require.config({
 	baseUrl: "",
-
 	paths: {
-		"moment": "scripts/vendor/momentjs/min/moment-with-locales",
-		"underscore": "scripts/vendor/underscore/underscore"
+		"moment": "scripts/vendor/momentjs/min/moment-with-locales"
 	}
 });
 
 require([
 	'moment'
 ], function (moment) {
-	moment.locale('en');
+	moment.locale();
 
-	var piMoment = moment("03-14-15 9:26:53", 'MM/DD/YY H:mm:ss');
+	var nextPiDay = findNextPiDay();
 
-	updateTimeUntilPi();
+	printTargetDate(nextPiDay);
+	updateTimeUntilTargetDate(nextPiDay);
 
-	document.getElementById("piMoment").innerHTML = piMoment.format('M/DD/YY, H:mm:ss');
+	function findNextPiDay () {
+		var nextPiDay = moment("03-14 00:00:00", 'MM/DD HH:mm:ss');
 
-	function updateTimeUntilPi () {
+		// Check if the next pi day is this year or next year.
+		if (moment().diff(nextPiDay, "days") < 1) {
+			nextPiDay.year(moment().year());
+		} else {
+			nextPiDay.year(moment().add(1, "year").year());
+		}
+
+		return nextPiDay;
+	}
+
+	function updateTimeUntilTargetDate (targetDate) {
 		var now =  moment();
-		console.log(getDuration(now, piMoment));
-		var durationString = getDurationString(getDuration(now, piMoment));
-		document.getElementById("timeUntilPi").innerHTML = durationString;
-		document.title = "π " + piMoment.from(now) + " | Super Pi Day countdown";
 
-		window.setTimeout(updateTimeUntilPi, 500);
+		if (isTargetDateToday(targetDate)) {
+			hideTimeElement();
+			showCelebrationElement();
+			document.title = "Happy π day!";
+		} else {
+			showTimeElement();
+			hideCelebrationElement();
+			var durationString = getDurationString(getDuration(now, targetDate));
+			document.getElementById("timeUntilTargetDate").innerHTML = durationString;
+			document.title = "π " + targetDate.from(now);
+
+			window.setTimeout(function () {
+				updateTimeUntilTargetDate(targetDate);
+			}, 500);
+		}
+	}
+
+	function isTargetDateToday (targetDate) {
+		if (moment().diff(targetDate, "days") === 0) {
+			return true;
+		}
+
+		return false;
 	}
 
 	function getDurationString (duration) {
@@ -97,21 +125,33 @@ require([
 	}
 
 	function getDuration (now, then) {
-		var differenceInSeconds = (then - now)/1000;
-		var secondsInMinute = 60;
+		var secondsInMinute,
+				secondsInHour,
+				secondsInDay,
+				minutesInHour,
+				hoursInDay,
+				differenceInSeconds,
+				dSecond,
+				dMinute,
+				dHour,
+				dDay,
+				duration;
 
-		var secondsInHour = 60*60;
-		var minutesInHour = 60;
+		secondsInMinute = 60;
 
-		var secondsInDay = 60*60*24;
-		var hoursInDay = 24;
+		secondsInHour = 60*60;
+		minutesInHour = 60;
 
-		var dSecond = Math.floor(differenceInSeconds) % secondsInMinute;
-		var dMinute = Math.floor(differenceInSeconds/secondsInMinute) % minutesInHour;
-		var dHour = 	Math.floor(differenceInSeconds/secondsInHour) % hoursInDay;
-		var dDay = 		Math.floor(differenceInSeconds/secondsInDay);
+		secondsInDay = 60*60*24;
+		hoursInDay = 24;
 
-		var duration = {
+		differenceInSeconds = (then - now)/1000;
+		dSecond = Math.floor(differenceInSeconds) % secondsInMinute;
+		dMinute = Math.floor(differenceInSeconds/secondsInMinute) % minutesInHour;
+		dHour = 	Math.floor(differenceInSeconds/secondsInHour) % hoursInDay;
+		dDay = 		Math.floor(differenceInSeconds/secondsInDay);
+
+		duration = {
 			DAYS: dDay,
 			HOURS: dHour,
 			MINUTES: dMinute,
@@ -119,5 +159,57 @@ require([
 		}
 
 		return duration;
+	}
+
+	function printTargetDate (targetDate) {
+		document.getElementById("targetDate").innerHTML = targetDate.format('MMMM Do, YYYY');
+	}
+
+	function showTimeElement () {
+		var timeElement = getTimeElementOrNull();
+
+		if (timeElement) {
+			timeElement.style.display = "block";
+		}
+	}
+
+	function hideTimeElement () {
+		var timeElement = getTimeElementOrNull();
+
+		if (timeElement) {
+			timeElement.style.display = "none";
+		}
+	}
+
+	function showCelebrationElement () {
+		var celebrationElement = getCelebrationElementOrNull();
+
+		if (celebrationElement) {
+			celebrationElement.style.display = "block";
+		}
+	}
+
+	function hideCelebrationElement () {
+		var celebrationElement = getCelebrationElementOrNull();
+
+		if (celebrationElement) {
+			celebrationElement.style.display = "none";
+		}
+	}
+
+	function getFirstElementWithClassNameOrNull (className) {
+		if (document.getElementsByClassName(className).length > 0) {
+			return document.getElementsByClassName(className)[0];
+		}
+
+		return null;
+	}
+
+	function getTimeElementOrNull () {
+		return getFirstElementWithClassNameOrNull("time");
+	}
+
+	function getCelebrationElementOrNull () {
+		return getFirstElementWithClassNameOrNull("celebration");	
 	}
 });
